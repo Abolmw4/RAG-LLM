3be8ac7bf753e5a837bd44d56d0893d94655d045
abolfazl <a.asghari251@gmail.com>
2026-07-02 21:59:07 +0330

    feat: Reading pdf's from directory and tested

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -1,28 +1,43 @@
 from langchain_core.documents import Document
 from langchain_community.document_loaders import PyPDFLoader
+from pathlib import Path
 from typing import List
 
 
-class DataIngestion:
-    def __init__(self, file_path: str) -> None:
-        self.file_path = file_path
-    
-    def _load_pdf(self) -> List[Document]:
-        print("info:", f"Loading PDF: {self.file_path.split('/')[-1]}")
 
-        loader = PyPDFLoader(str(self.file_path))
+class DataIngestion:
+    SUPPORTED_FILES = ['.pdf']
+    def __init__(self, path: str) -> None:
+        self.path = Path(path)
+    
+    def _load_pdf(self, pdf_path: Path) -> List[Document]:
+        print("info:", f"Loading: {pdf_path.name}")
+
+        loader = PyPDFLoader(str(pdf_path))
 
         documents = loader.load()
+
+        for doc in documents:
+            doc.metadata["source"] = pdf_path.name
 
         print("info:", f"Loaded {len(documents)} pages.")
 
         return documents
     
-    def load_document(self) -> List[Document]:
-        suffix = self.file_path.split('/')[-1].split('.')[-1]
-        match suffix:
-            case "pdf":
-                return self._load_pdf()
-            case _:
-                print("Unknow file")        
-        
+    def load_documents(self) -> List[Document]:
+        documents = []
+
+        if self.path.is_file():
+            if self.path.suffix.lower() not in self.SUPPORTED_FILES:
+                raise ValueError(f"Unsupported file: {self.path.suffix}")
+            return self._load_pdf(self.path)
+
+        elif self.path.is_dir():
+            pdf_files = sorted(self.path.glob("*.pdf"))
+            if not pdf_files:
+                raise ValueError("No PDF files found.")
+            for pdf in pdf_files:
+                documents.extend(self._load_pdf(pdf))
+            return documents
+        else:
+            raise RuntimeError("Unexpected path.")
--- test/test_data_ingestion.py
+++ test/test_data_ingestion.py
@@ -1,13 +1,39 @@
 import unittest
 from src.data_ingestion import DataIngestion
 
+DATA_DIR = "/home/abolfazl/Documents/rag-llm-project/docs"
+
 class MyTestCase(unittest.TestCase):
-    def test_load_pdf_file(self):
-        data_igst = DataIngestion(file_path="/home/abolfazl/Documents/rag-llm-project/docs/sodt+++.pdf")
-        result = data_igst.load_document()
+    data_igst = DataIngestion(DATA_DIR)
+    result = data_igst.load_documents()
+    
+    def test_documents_are_loaded(self):
+        cls = type(self)
+        result = cls.data_igst.load_documents()
+        self.assertGreater(len(result), 0)
 
-        print(result[0].metadata)
+    def test_document_type(self):
+        cls = type(self)
+        from langchain_core.documents import Document
+        self.assertIsInstance(cls.result[0], Document)
 
+    def test_page_content_is_not_empty(self):
+        cls = type(self)
+        self.assertTrue(len(cls.result[0].page_content.strip()) > 0)
 
+    def test_metadata_contains_required_fields(self):
+        cls = type(self)
+        metadata = cls.result[0].metadata
+
+        self.assertIn("source", metadata)
+        self.assertIn("page", metadata)
+        self.assertIn("page_label", metadata)
+        self.assertIn("total_pages", metadata)
+
+    def test_source_is_pdf(self):
+        cls = type(self)
+        source = cls.result[0].metadata["source"]
+        self.assertTrue(source.endswith(".pdf"))
+        
 if __name__ == '__main__':
     unittest.main()
