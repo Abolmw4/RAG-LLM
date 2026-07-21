d749e4abc116829e8cf78d10145824bae5d9e350
abolfazl <a.asghari251@gmail.com>
2026-07-21 20:48:36 +0330

    feat: Add hybrid retriver into pipeline and main file and then tested

--- src/pipeline/simple_rag_pipeline.py
+++ src/pipeline/simple_rag_pipeline.py
@@ -2,13 +2,18 @@ from src.pipeline.pipeline import PipeLine
 from src.retriver.retriver_base import Retriver 
 from src.prompt_builder.prompt_builder import PromptBuilder
 from src.generators.generator import Generator
+from src.fusion.base_fusion import BaseFusion
+from src.fusion.rrffusion import RRFFusion
+from src.schemas.retrived_document import RetrivedDocument
 from langchain_core.documents import Document
 from typing import List
 from src.utils.utils import logger
 class SimpleRAGPipeLine(PipeLine):
-    def __init__(self, retriver: Retriver, prompt_builder: PromptBuilder, generator: Generator):
+    def __init__(self, retriver_dense: Retriver, retriver_spars: Retriver, fusion: BaseFusion, prompt_builder: PromptBuilder, generator: Generator):
         super().__init__()
-        self.retriver = retriver
+        self.retriver_dense = retriver_dense
+        self.retriver_sparse = retriver_spars
+        self.fusion = fusion
         self.prompt_builder = prompt_builder
         self.generator = generator
         
@@ -16,7 +21,12 @@ class SimpleRAGPipeLine(PipeLine):
         if not question.strip():
             logger.error(f"{__class__.__name__}: question is empty")
             raise ValueError("question not be empty")
-        result: List[Document] = self.retriver.retrive(question, k=k)
+        
+        # result: List[Document] = self.retriver.retrive(question, k=k)
+        result_dense: List[RetrivedDocument] = self.retriver_dense.retrive(question, k=k)
+        result_spars: List[RetrivedDocument] = self.retriver_sparse.retrive(question, k=k)
+        
+        result: List[RetrivedDocument] = self.fusion.merge(dense_results=result_dense, sparse_results=result_spars, k=k)
         
         prompt = self.prompt_builder.build_prompt(question=question, docs=result)
         
--- src/prompt_builder/simple_prompt.py
+++ src/prompt_builder/simple_prompt.py
@@ -1,5 +1,5 @@
 from src.prompt_builder.prompt_builder import PromptBuilder
-from langchain_core.documents import Document
+from src.schemas.retrived_document import RetrivedDocument
 from typing import List
 from src.utils.utils import logger
 class SimplePrompt(PromptBuilder):
@@ -7,7 +7,7 @@ class SimplePrompt(PromptBuilder):
         super().__init__()
         pass
     
-    def build_prompt(self, question: str, docs: List[Document]) -> str:
+    def build_prompt(self, question: str, docs: List[RetrivedDocument]) -> str:
         if not question.strip():
             logger.error(f"{__class__.__name__}: question is empty")
             raise ValueError(f"question can not be emtpy")
@@ -16,7 +16,7 @@ class SimplePrompt(PromptBuilder):
             logger.error(f"{__class__.__name__}: documents is empty")
             raise ValueError(f"documents cannot be empty.")
         
-        context = "\n\n".join(doc.page_content for doc in docs)
+        context = "\n\n".join(doc.document.page_content for doc in docs)
         
         prompt = f"""
         تو یک دستیار هوش مصنوعی هستی که وظیفه داری بر اساس متن و اطلاعاتی که در اختیارت قرار میگیرد پاسخ دقیق و واضح بدهی. اگر جواب توی متن و اطلاعاتی که بهت داده شده نبود خیلی واضح بگو نمیدانم اطلاعی ندارم.
